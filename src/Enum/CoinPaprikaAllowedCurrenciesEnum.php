8753372aa6df3b467366a4da636a5360eabe4174
Maral Tim <maralbars@gmail.com>
2024-07-17 20:11:02 +0500

    Add enums with allowed currencies

---
+++ src/Enum/CoinPaprikaAllowedCurrenciesEnum.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Enum;
+
+enum CoinPaprikaAllowedCurrenciesEnum: string
+{
+    case BTC = 'BTC';
+    case ETH = 'ETH';
+    case USD = 'USD';
+    case EUR = 'EUR';
+    case PLN = 'PLN';
+    case KRW = 'KRW';
+    case GBP = 'GBP';
+    case CAD = 'CAD';
+    case JPY = 'JPY';
+    case RUB = 'RUB';
+    case TRY = 'TRY';
+    case NZD = 'NZD';
+    case AUD = 'AUD';
+    case CHF = 'CHF';
+    case UAH = 'UAH';
+    case HKD = 'HKD';
+    case SGD = 'SGD';
+    case NGN = 'NGN';
+    case PHP = 'PHP';
+    case MXN = 'MXN';
+    case BRL = 'BRL';
+    case THB = 'THB';
+    case CLP = 'CLP';
+    case CNY = 'CNY';
+    case CZK = 'CZK';
+    case DKK = 'DKK';
+    case HUF = 'HUF';
+    case IDR = 'IDR';
+    case ILS = 'ILS';
+    case INR = 'INR';
+    case MYR = 'MYR';
+    case NOK = 'NOK';
+    case PKR = 'PKR';
+    case SEK = 'SEK';
+    case TWD = 'TWD';
+    case ZAR = 'ZAR';
+    case VND = 'VND';
+    case BOB = 'BOB';
+    case COP = 'COP';
+    case PEN = 'PEN';
+    case ARS = 'ARS';
+    case ISK = 'ISK';
+}
